1d150769f1b7099cf1b0b5c1aa2d7026f5278c18
John McGuinness <johnmcguinness@johns-mbp.dhcp.nd.edu>
2018-03-20 23:08:20 -0400

    randomized model working

--- my_portfolio.py
+++ my_portfolio.py
@@ -15,11 +15,24 @@ class Generator(PortfolioGenerator):
 	
 	def __init__(self):
 		pass
-	
-	#def build_signal(self, stock_features):
+
+	# for now this just returns random weights
+	def build_signal(self, stock_features):
+		ticker_vals = stock_features['ticker'].unique()
+		weights = 200*np.random.rand(ticker_vals.shape[0])-100
+		output = pd.Series(weights, index=ticker_vals)
+		return output
 		
 
 #main for testing
 if __name__=='__main__':
 	gen = Generator()
-	gen.read_stock_data()
+	stock_df = gen.read_stock_data()
+	'''
+	ticker_vals = stock_df['ticker'].unique()
+	weights = 200*np.random.rand(ticker_vals.shape[0])-100
+	output = pd.Series(weights, index=ticker_vals)
+	print output
+	'''
+	gen.build_signal(stock_df)
+	print gen.simulate_portfolio()
